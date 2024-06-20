689ad2e07031ce9713d0dcb515966933d36b49da
jennywohlfahrtstaetter <162671255+jennywohlfahrtstaetter@users.noreply.github.com>
2024-06-20 17:19:51 +1000

    responsive design

--- scripts.js
+++ scripts.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
+
+  $(".hamburger-button").click(function(){
+
+    $(this).toggleClass('active');
+    $('.mob-menu').slideToggle();
+  });
+
   $('.slick-carousel').slick({
     dots: true,
     arrows: true,
     infinite: true,
-    autoplay: true,
+    autoplay: false,
     autoplaySpeed: 5000,
     speed: 1000,
     accessibility: true,
@@ -11,7 +18,28 @@ $(document).ready(function(){
     pauseOnHover: false,
     pauseOnDotsHover: true,
     slidesToShow: 5,
-    slidesToScroll: 5
+    slidesToScroll: 5,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          dots: true,
+          arrows: false
+        }
+      }
+    ]
   });
 });
-		
